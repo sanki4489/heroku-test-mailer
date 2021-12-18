910a8654b1b8da9a1fdf8ab4357591545db38416
Sankalp Verma <sunklapverma4489@gmail.com>
2021-12-18 10:39:56 +0530

    added mailer services sendgrid

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.confrimation.subject
+  #
+  def confrimation(user)
+    @user = user
+    @greeting = "Hi #{@user.name}"
+
+    mail to: @user.email, subject: @gretting
+  end
+end
